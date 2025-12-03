c15a1b5c006aa0dcd1bbfe7600fc755cfed40716
dawg0801 <aritra0801@gmail.com>
2025-12-03 10:52:14 +0530

    container config done

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,1 +1,29 @@
-console.log("Harsh Verma");
+// let score = "33"
+// let score = "33abc"
+// let score = null
+// let score = undefined
+let score = true
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);       // "33" => 33
+                               // "33abc" => NaN(not a number)
+                                  // null => 0
+                             // undefined => NaN                          
+                            // true/false => 1/0  
+
+let isLoggedIn = "Aritra"
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn)
+
+// 1 => true // 0 => false
+// "" => false // "Aritra" => true
+
+let someNumber = 33
+
+
