fd64bf41892d235d2b8d182b19d53ee26c2105fd
dawg0801 <aritra0801@gmail.com>
2025-12-27 19:01:44 +0530

    container config done

--- 01_basics/06_datatypes_summary.js
+++ 01_basics/06_datatypes_summary.js
@@ -39,3 +39,28 @@ const myFunction = function(){ // Declaration of a function
 console.log(typeof bigNumber);// typeof is a pre-defined function
 // all non-primitive datatype have object as datatype
 // function also has datatype as object function 
+
+// *******************************************************************
+
+// two types of memory allocation
+// Stack (Primitive) , Heap (Non-primitive)
+
+let myYoutubename = "aritramondaldotcom"
+
+let anothername = myYoutubename
+anothername = "chaiaurcode"// and the copy got updated
+
+console.log(myYoutubename);// all primitive values are taken in stack
+console.log(anothername);// in stack only a copy is given
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl",
+}
+
+let userTwo = userOne
+
+userTwo.email = "aritra@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
