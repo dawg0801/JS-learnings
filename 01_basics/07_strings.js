fb57041f5829457117d4cfd1d4618d56d68469b2
dawg0801 <aritra0801@gmail.com>
2026-04-09 18:06:07 +0530

    cantainer config done

---
+++ 01_basics/07_strings.js
@@ -0,0 +1,9 @@
+const name = "Aritra"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value"); never write like this
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+// use back-ticks(``) its the modern way of syntax
+
+const gameName = new String("Doom") // declaration of a string
